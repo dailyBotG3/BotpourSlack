fc9cf3b6d778bb7bda67de40cb0ebba262dd1960
Sana <80457921+sana-aliniaa@users.noreply.github.com>
2022-02-16 10:57:22 +0100

    dateWorks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { WebClient, LogLevel } = require("@slack/web-api");
 
 // WebClient instantiates a client that can call API methods
 // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient("xoxb-3118916858705-3112436425635-Lb7d7gl1IDFpeAdByVgbVJXf", {
+const client = new WebClient("xoxb-3118916858705-3112436425635-4U1pdkyLmOYzp5envprEFIBY", {
   // LogLevel can be imported and used to make debugging simpler
   logLevel: LogLevel.DEBUG
 });
@@ -16,7 +16,7 @@ async function publishMessage(id, text) {
       // Call the chat.postMessage method using the built-in WebClient
       const result = await client.chat.postMessage({
         // The token you used to initialize your app
-        token: "xoxb-3118916858705-3112436425635-Lb7d7gl1IDFpeAdByVgbVJXf",
+        token: "xoxb-3118916858705-3112436425635-4U1pdkyLmOYzp5envprEFIBY",
         channel: id,
         text: text
         // You could also use a blocks[] array to send richer content
@@ -30,4 +30,7 @@ async function publishMessage(id, text) {
     }
   }
   
-  publishMessage("C032PLGFD2T", "Hello world :tada:");
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+  publishMessage("C0336J5RLF6", `Daily of the ${date}`);
